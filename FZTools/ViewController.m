869abb535147c6a58a36c5c464a4e4eaa98f4896
xsh <13213638904@163.com>
2020-10-20 10:13:15 +0800

    first commit

--- FZTools/ViewController.m
+++ FZTools/ViewController.m
@@ -16,6 +16,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.title = @"Object";
+    self.view.backgroundColor = [UIColor redColor];
 }
 
 
